7b481586fe812ee3f4bdb1409969267f7019903a
michael <457813723@qq.com>
2023-10-20 14:27:20 +0800

    remove debug

--- tool/twitter.go
+++ tool/twitter.go
@@ -3,6 +3,7 @@ package tool
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/parnurzeal/gorequest"
 	"time"
 )
@@ -36,13 +37,18 @@ func TweetsSearch(token, twitterName string) (resp TweetsSearchResp, err error) 
 	url := fmt.Sprintf(TwitterSearchUrl, twitterName)
 	_, body, errs := gorequest.New().Get(url).Set("Authorization", fmt.Sprintf("Bearer %s", token)).Timeout(time.Second * 10).End()
 	if len(errs) > 0 {
-		fmt.Println("sendLarkTextNotify req err:", errs)
+		fmt.Println("TweetsSearch curl twitter err:", errs)
 		err = fmt.Errorf("gorequest err :", errs[0].Error())
+		return
 	}
+
 	err = json.Unmarshal([]byte(body), &resp)
 	if err != nil {
-		fmt.Println("json.Unmarshal err:", err.Error())
+		fmt.Println("TweetsSearch json.Unmarshal err:", err.Error())
 		return
+	}
+	if len(resp.Data) == 0 {
+		log.Info("twitter api response: ", resp)
 	}
 	return
 }
