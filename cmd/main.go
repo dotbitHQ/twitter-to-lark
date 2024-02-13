c21039586c149cf24127ec6c5ddce9a259411c3d
michael <457813723@qq.com>
2024-02-13 12:59:22 +0800

    redis test

--- cmd/main.go
+++ cmd/main.go
@@ -62,7 +62,12 @@ func runServer(ctx *cli.Context) error {
 		log.Info("redis ok")
 	}
 	rc := cache.Initialize(red)
-
+	if err := rc.SetTweets2lark("test11"); err != nil {
+		fmt.Println("set redis err11111")
+	} else {
+		fmt.Println("set redis ok22222")
+	}
+	return nil
 	// task
 	twitterTask := task.Task{
 		Ctx: ctxServer,
